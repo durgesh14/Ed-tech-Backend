ac1831c46377f51a1e04a9e330a781ab94b9f875
durgesh14 <34915741+durgesh14@users.noreply.github.com>
2023-07-01 22:34:23 +0530

    vercel test 5

--- api/server.js
+++ api/server.js
@@ -4,6 +4,7 @@ const cors = require("cors");
 const authRoutes = require("../routes/auth.routes");
 const sectionRoutes = require("../routes/section.routes");
 const path = require("path");
+const PORT = process.env.PORT || 8000;
 
 app.use(cors());
 
@@ -16,12 +17,12 @@ const db = require("../config/mongoose");
 app.use("/api/auth", authRoutes);
 app.use("/api/lesson", sectionRoutes);
 
-app.listen(port, function (err) {
+app.listen(PORT, function (err) {
   if (err) {
     console.log(`Error in running server: ${err}`);
   }
 
-  console.log(`Server is running on port: ${port}`);
+  console.log(`Server is running on port: ${PORT}`);
 });
 
 module.exports = app;
